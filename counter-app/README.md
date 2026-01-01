b01e45e0da5e2a9dff45724747d34c8e3f875c7d
Prabhanjan R <prabhanjan1297@gmail.com>
2026-01-02 00:51:23 +0530

    Counter value update to local storage, Messages coloring update

--- counter-app/README.md
+++ counter-app/README.md
@@ -5,5 +5,8 @@ Simple counter app built using JavaScript.
 ## Features
 - Increment counter
 - Decrement counter
+- Save Value to Local Storage
+- Reset Value
+- Proper Messages based on each button click
 
 ![counter-app-image](image.png)
--- counter-app/script.js
+++ counter-app/script.js
@@ -1,4 +1,5 @@
 document.querySelector('.increment-button').addEventListener('click', function() {
+        document.querySelector('.message').style.color = 'black';
     document.querySelector('.message').innerText = "Message will display here!";
     let count = parseInt(document.querySelector('.counter-value').innerText);
     count++;
@@ -6,11 +7,13 @@ document.querySelector('.increment-button').addEventListener('click', function()
 });
 
 document.querySelector('.decrement-button').addEventListener('click', function() {
+        document.querySelector('.message').style.color = 'black';
     document.querySelector('.message').innerText = "Message will display here!";
     let count = parseInt(document.querySelector('.counter-value').innerText);
     count--;
     if(count < 0) {
         count = 0;
+        document.querySelector('.message').style.color = 'red';
         document.querySelector('.message').innerText = "Counter cannot go below zero!";
     }
     document.querySelector('.counter-value').innerText = count;
@@ -18,15 +21,24 @@ document.querySelector('.decrement-button').addEventListener('click', function()
 
 document.querySelector('.reset-button').addEventListener('click', function() {
     if( parseInt(document.querySelector('.counter-value').innerText) === 0 ) {
+        document.querySelector('.message').style.color = 'red';
         document.querySelector('.message').innerText = "Counter is already at zero.";
         return;
     }
+    document.querySelector('.message').style.color = 'black';
     document.querySelector('.message').innerText = "Counter has been reset.";
     document.querySelector('.counter-value').innerText = 0;
 });
 
 document.querySelector('.save-button').addEventListener('click', function() {
     let counter = document.querySelector('.counter-value').innerText;
-    document.querySelector('.message').innerText = `${counter} value saved successfully!`;
+    if(counter == 0) {
+        document.querySelector('.message').style.color = '#cfcb11';
+        document.querySelector('.message').innerText = "Cannot save a zero value.";
+        return;
+    }
+    localStorage.setItem('savedCounter', counter);
+        document.querySelector('.message').style.color = 'green';
+    document.querySelector('.message').innerText = `${counter} value saved to local storage successfully.`;
     document.querySelector('.counter-value').innerText = 0;
 });
