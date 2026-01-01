0fbc7ac80d3bacc8792b82af70ae37dde5409230
Prabhanjan R <prabhanjan1297@gmail.com>
2026-01-02 00:23:18 +0530

    Added Save, Reset Functionalities with proper message

--- counter-app/script.js
+++ counter-app/script.js
@@ -1,11 +1,32 @@
 document.querySelector('.increment-button').addEventListener('click', function() {
+    document.querySelector('.message').innerText = "Message will display here!";
     let count = parseInt(document.querySelector('.counter-value').innerText);
     count++;
     document.querySelector('.counter-value').innerText = count;
 });
 
 document.querySelector('.decrement-button').addEventListener('click', function() {
+    document.querySelector('.message').innerText = "Message will display here!";
     let count = parseInt(document.querySelector('.counter-value').innerText);
     count--;
+    if(count < 0) {
+        count = 0;
+        document.querySelector('.message').innerText = "Counter cannot go below zero!";
+    }
     document.querySelector('.counter-value').innerText = count;
+});
+
+document.querySelector('.reset-button').addEventListener('click', function() {
+    if( parseInt(document.querySelector('.counter-value').innerText) === 0 ) {
+        document.querySelector('.message').innerText = "Counter is already at zero.";
+        return;
+    }
+    document.querySelector('.message').innerText = "Counter has been reset.";
+    document.querySelector('.counter-value').innerText = 0;
+});
+
+document.querySelector('.save-button').addEventListener('click', function() {
+    let counter = document.querySelector('.counter-value').innerText;
+    document.querySelector('.message').innerText = `${counter} value saved successfully!`;
+    document.querySelector('.counter-value').innerText = 0;
 });
